860b13cc06d98f84e879fe5ead0003ec2e0baa3d
David Lee <mikeportnoy168@gmail.com>
2026-04-22 17:09:32 +0800

    feat: create Freezed entities for TMDB Movie data

---
+++ lib/src/features/movies/domain/movie.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'movie.freezed.dart';
+part 'movie.g.dart';
+
+@freezed
+class Movie with _$Movie {
+  const factory Movie({
+    required int id,
+    required String title,
+    required String overview,
+    @JsonKey(name: 'poster_path') String? posterPath,
+    @JsonKey(name: 'backdrop_path') String? backdropPath,
+    @JsonKey(name: 'vote_average') required double voteAverage,
+    @JsonKey(name: 'release_date') String? releaseDate,
+  }) = _Movie;
+
+  factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
+}
---
+++ lib/src/features/movies/domain/movie.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$MovieImpl _$$MovieImplFromJson(Map<String, dynamic> json) => _$MovieImpl(
+  id: (json['id'] as num).toInt(),
+  title: json['title'] as String,
+  overview: json['overview'] as String,
+  posterPath: json['poster_path'] as String?,
+  backdropPath: json['backdrop_path'] as String?,
+  voteAverage: (json['vote_average'] as num).toDouble(),
+  releaseDate: json['release_date'] as String?,
+);
+
+Map<String, dynamic> _$$MovieImplToJson(_$MovieImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'overview': instance.overview,
+      'poster_path': instance.posterPath,
+      'backdrop_path': instance.backdropPath,
+      'vote_average': instance.voteAverage,
+      'release_date': instance.releaseDate,
+    };
