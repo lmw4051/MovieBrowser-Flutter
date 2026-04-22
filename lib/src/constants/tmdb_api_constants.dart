471b144ab7e92f128ba837b768ccee9a4d756ac0
David Lee <mikeportnoy168@gmail.com>
2026-04-22 19:06:55 +0800

    feat: add TmdbApiConstants

---
+++ lib/src/constants/tmdb_api_constants.dart
@@ -0,0 +1,5 @@
+class TmdbApiConstants {
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/';
+  static const String posterSize = 'w200';
+  static const String backdropSize = 'w780';
+}
