5b04f3058968024cafff271e8a56c47f2fea09f6
David Lee <mikeportnoy168@gmail.com>
2026-04-23 22:35:03 +0800

    chore: setup Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movie_browser/src/routing/app_router.dart';
 
-void main() {
+void main() async {
+  // 💡 Ensure Flutter bindings are initialized
+  // (since we are performing async operations before runApp)
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 💡 Initialize Hive and open a box named 'favorites'
+  // We specify the type as <String>
+  // because we will store the Movie objects as JSON strings
+  await Hive.initFlutter();
+  await Hive.openBox<String>('favorites');
+
   runApp(
     const ProviderScope(
       child: MyApp(),
