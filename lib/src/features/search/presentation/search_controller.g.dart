91b1834b487336d216e6d1b41627cb669b7b6872
David Lee <mikeportnoy168@gmail.com>
2026-04-24 16:58:10 +0800

    feat: add SearchController

---
+++ lib/src/features/search/presentation/search_controller.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$searchControllerHash() => r'bb15eae1ba101c370e97d7d73ab153eccf31e855';
+
+/// See also [SearchController].
+@ProviderFor(SearchController)
+final searchControllerProvider =
+    AutoDisposeNotifierProvider<
+      SearchController,
+      AsyncValue<List<Movie>>
+    >.internal(
+      SearchController.new,
+      name: r'searchControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$searchControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SearchController = AutoDisposeNotifier<AsyncValue<List<Movie>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
